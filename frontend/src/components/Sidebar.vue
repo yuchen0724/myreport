7f69ccc2e81d3b12270a95118db1e6374084371d
yuchen0724 <yuchen0724@users.noreply.github.com>
2026-04-21 23:35:54 +0800

    feat: 添加第二阶段功能导航入口

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -19,6 +19,18 @@
         <el-icon><document /></el-icon>
         <span>SQL 查询</span>
       </el-menu-item>
+      <el-menu-item index="/nl2sql">
+        <el-icon><chat-line-round /></el-icon>
+        <span>NL2SQL 查询</span>
+      </el-menu-item>
+      <el-menu-item index="/charts">
+        <el-icon><trend-charts /></el-icon>
+        <span>图表查看器</span>
+      </el-menu-item>
+      <el-menu-item index="/templates">
+        <el-icon><folder /></el-icon>
+        <span>模板管理</span>
+      </el-menu-item>
     </el-menu>
   </div>
 </template>
@@ -26,11 +38,11 @@
 <script>
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
-import { House, DataLine, Document } from '@element-plus/icons-vue'
+import { House, DataLine, Document, ChatLineRound, TrendCharts, Folder } from '@element-plus/icons-vue'
 
 export default {
   name: 'Sidebar',
-  components: { House, DataLine, Document },
+  components: { House, DataLine, Document, ChatLineRound, TrendCharts, Folder },
   setup() {
     const route = useRoute()
     const activeMenu = computed(() => route.path)
